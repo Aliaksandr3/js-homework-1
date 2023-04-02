475ffec4d539ed418e228f7236722a7d16b0e6b2
Черный Александр <112432741+Aliaksandr3@users.noreply.github.com>
2023-04-02 19:16:05 +0300

    Commit 02.04.23

---
+++ js/app.js
@@ -0,0 +1,59 @@
+// lesson 9 task 1
+// let text = prompt("Введите текст"," ");
+// alert (text)
+
+// lesson 9 task 2
+// let firstNumber = +prompt("Введите первое число",' ');
+// let secondNumber = +prompt("Введите второе число",' ');
+// let thirdNumber = +prompt("Введите третье число",' ');
+// let sum = firstNumber+secondNumber+thirdNumber;
+// alert(sum) 
+
+// lesson 9 task 3
+// const name = prompt("Введите имя пользователя",' ');
+// let age = prompt("Введите возраст пользователя, полных лет",' ');
+// if (age >= 18) {alert('Добро пожаловать')} 
+// else {alert("Простите, "+(name)+" , доступ закрыт!")}
+
+// lesson 9 task 4
+// let bl = confirm ("Хотите ввести название блока?"), nameBl;
+//     if (bl == true)
+//         {
+//             nameBl = prompt ("Введите название блока", "")
+//                 let el = confirm ("Хотите ввести название элемента?"), nameEl;
+//                     if (el == true)
+//                         {
+//                             nameEl = prompt ("Введите название элемента", "")
+//                             let mod = confirm ("Хотите ввести название модификатора?"), nameMod;
+//                                 if (mod == true)
+//                                     {
+//                                         nameMod = prompt ("Введите название модификатора", "")
+//                                         alert ((nameBl) + '_' + (nameEl) + '__' + (nameMod))
+//                                     }   
+//                                 else 
+//                                     {
+//                                         alert ((nameBl)+ '_' +(nameEl))
+//                                     }
+//                         }
+//                     else 
+//                         {
+//                             alert (nameBl)
+//                         }
+//         }   
+//     else 
+//         {
+//             alert ("Элементу не присвоен класс!","")
+//         }
+
+// lesson 9 task 5 
+// let a = prompt ('Введите первое число','');
+// let b = prompt ('Введите второе число','');
+// let res = prompt ('Выберите нужную операцию (div, plus, mult, minus)','');
+// if (res == "div") 
+// alert (+a / +b)
+// if (res == "plus") 
+// alert (+a + +b)
+// if (res == "mult") 
+// alert (+a * +b)
+// if (res == "minus") 
+// alert (+a - +b)
